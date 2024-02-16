84a6935f2a427e831f2218a9bc9ad39bd571992a
Abraham Muldrow <abe@muldrow.net>
2024-02-15 17:11:17 -0800

    Create lab3_notes.py

---
+++ src/lab3_notes.py
@@ -0,0 +1,25 @@
+
+# Notes from the beginning of lab
+# import the different classes we made
+# return the position instead of printing the position
+
+# add a gui and controller to adjust the controller
+    # are we going to have do a bunch of stuff
+
+# ten times the time constant portportional control
+    # will just have to do a P control 
+    # 50 to 100 calls at a time
+# .1 second time response 
+
+# set point, where do we want to be 
+# belonging to controller class
+# self.set_point
+
+# give the controllers constructor a callable
+# give the functon to the controller option so it can use the function varible to read it in the class and functions
+
+# know hwere the encoder is
+# have the function callable so the controller could work for other things 
+
+# import motor driver 
+# control algorithim several times a second
